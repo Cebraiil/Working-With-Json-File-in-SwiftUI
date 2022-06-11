304330c10c0a862598d62480b34033233e9c32c6
Dev Light <106925514+developerlight@users.noreply.github.com>
2022-06-11 14:41:21 +0300

    Initial Commit

---
+++ WorkingWithJson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkingWithJson
+//
+//  Created by Abdul Adl on 11.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorkingWithJson/WorkingWithJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithJsonApp.swift
+//  WorkingWithJson
+//
+//  Created by Abdul Adl on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
